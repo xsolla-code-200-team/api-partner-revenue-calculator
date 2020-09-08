a43a77ec17aa6b5c4548e446494f324b2683f199
Arseny Neustroev <neustroev.arseny@gmail.com>
2020-09-08 13:30:23 +0500

    initial commit

---
+++ xsolla-revenue-calculator/Controllers/RevenueForecastController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace xsolla_revenue_calculator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RevenueForecastController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
