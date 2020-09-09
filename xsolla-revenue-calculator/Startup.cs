0af571aea7855a6873fb2898e98b26607ed722d4
Arseny Neustroev <neustroev.arseny@gmail.com>
2020-09-09 21:10:56 +0500

    added CORS policy

--- xsolla-revenue-calculator/Startup.cs
+++ xsolla-revenue-calculator/Startup.cs
@@ -54,6 +54,7 @@ namespace xsolla_revenue_calculator
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(builder => builder.AllowAnyOrigin());
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
