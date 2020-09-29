85d9bb15ad92c3271bd984c409df8783fe4fc01f
Arseny Neustroev <neustroev.arseny@gmail.com>
2020-09-29 12:25:16 +0500

    fix specs example values

--- xsolla-revenue-calculator/Models/UserInfoModels/UserInfoFullRequestBody.cs
+++ xsolla-revenue-calculator/Models/UserInfoModels/UserInfoFullRequestBody.cs
@@ -18,13 +18,13 @@ namespace xsolla_revenue_calculator.Models.UserInfoModels
                 ProductName = "Super game",
                 Genres = new List<string> {"rpg", "action"},
                 Monetization = "free2play",
-                Platforms = new List<string> {"macos", "ios"},
-                Regions = new List<string> {"1", "5", "10"},
+                Platforms = new List<string> {"mac", "ios"},
+                Regions = new List<string> {"1", "10"},
                 CompanyName = "Super developer",
                 Email = "super-developer@email.com",
                 Cost = 100000,
                 Sales = 50,
-                ReleaseDate = "April-June"
+                ReleaseDate = "april-june"
             };
         }
     }
